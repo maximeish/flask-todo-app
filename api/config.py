7de5b9ff5aa64ed91e07b8a7e44e70ec78e412e3
Maxime Ishimwe <mxmishimwe5@gmail.com>
2021-11-19 00:34:50 +0200

    add login feature

--- api/config.py
+++ api/config.py
@@ -19,15 +19,6 @@ class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
 
 
-class TestingConfig(BaseConfig):
-    """Testing configuration."""
-    DEBUG = True
-    TESTING = True
-    BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test'
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
-
-
 class ProductionConfig(BaseConfig):
     """Production configuration."""
     SECRET_KEY = 'my_precious'
--- api/models.py
+++ api/models.py
@@ -13,6 +13,113 @@ from flask.views import MethodView
 
 auth_blueprint = Blueprint('auth', __name__)
 
+app = Flask(__name__)
+CORS(app)
+
+app_settings = os.getenv(
+    'APP_SETTINGS',
+    'config.DevelopmentConfig'
+)
+app.config.from_object(app_settings)
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(
+            password, app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Validates the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, app.config.get(
+                'SECRET_KEY'), algorithms=['HS256'])
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
+        except Exception as e:
+            print(e)
+            return 'Invalid token. Please log in again.'
+
+
+class Task(db.Model):
+    """ Task Model for storing tasks for users """
+    __tablename__ = "tasks"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    u_id = db.Column(db.Integer, unique=False, nullable=False)
+    task = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, u_id, task, status=False):
+        self.u_id = u_id
+        self.task = task
+        self.status = status
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
+
 
 class SignupAPI(MethodView):
     """
@@ -23,7 +130,15 @@ class SignupAPI(MethodView):
         # get the post data
         post_data = request.get_json()
         # check if user already exists
-        user = User.query.filter_by(email=post_data.get('email')).first()
+        try:
+            user = User.query.filter_by(email=post_data.get('email')).first()
+        except Exception as e:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide email and password'
+            }
+            return make_response(jsonify(responseObject)), 401
+
         if not user:
             try:
                 user = User(
@@ -33,18 +148,21 @@ class SignupAPI(MethodView):
                 # insert the user
                 db.session.add(user)
                 db.session.commit()
+                print('now encoding token')
                 # generate the auth token
                 auth_token = user.encode_auth_token(user.id)
+                print('received auth_token ', auth_token)
                 responseObject = {
                     'status': 'success',
-                    'message': 'Successfully registered.',
-                    'auth_token': auth_token.decode()
+                    'message': 'User with id {0} successfully registered.'.format(user.decode_auth_token(auth_token)),
+                    'auth_token': auth_token
                 }
                 return make_response(jsonify(responseObject)), 201
             except Exception as e:
+                print(e)
                 responseObject = {
                     'status': 'fail',
-                    'message': 'Some error occurred. Please try again.'
+                    'message': 'Error in signing up user'
                 }
                 return make_response(jsonify(responseObject)), 401
         else:
@@ -76,7 +194,7 @@ class LoginAPI(MethodView):
                     responseObject = {
                         'status': 'success',
                         'message': 'Successfully logged in.',
-                        'auth_token': auth_token.decode()
+                        'auth_token': auth_token
                     }
                     return make_response(jsonify(responseObject)), 200
             else:
@@ -89,7 +207,7 @@ class LoginAPI(MethodView):
             print(e)
             responseObject = {
                 'status': 'fail',
-                'message': 'Try again'
+                'message': 'Provide a valid email and password'
             }
             return make_response(jsonify(responseObject)), 500
 
@@ -115,6 +233,7 @@ class UserAPI(MethodView):
             auth_token = ''
         if auth_token:
             resp = User.decode_auth_token(auth_token)
+
             if not isinstance(resp, str):
                 user = User.query.filter_by(id=resp).first()
                 responseObject = {
@@ -213,18 +332,6 @@ auth_blueprint.add_url_rule(
     methods=['POST']
 )
 
-app = Flask(__name__)
-CORS(app)
-
-app_settings = os.getenv(
-    'APP_SETTINGS',
-    'config.DevelopmentConfig'
-)
-app.config.from_object(app_settings)
-
-bcrypt = Bcrypt(app)
-db = SQLAlchemy(app)
-
 
 @app.route('/api')
 def index():
@@ -233,104 +340,19 @@ def index():
         200,
     )
 
+# 404 Not Found
+
+
+@app.errorhandler(404)
+def not_found(error):
+    response = make_response(
+        jsonify({"message": "Not Found"}),
+        404,
+    )
+    return response
+
 
 app.register_blueprint(auth_blueprint)
-
-
-class User(db.Model):
-    """ User Model for storing user related details """
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-
-    def __init__(self, email, password, admin=False):
-        self.email = email
-        self.password = bcrypt.generate_password_hash(
-            password, app.config.get('BCRYPT_LOG_ROUNDS')
-        ).decode()
-
-    def encode_auth_token(self, user_id):
-        """
-        Generates the Auth Token
-        :return: string
-        """
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
-            }
-            return jwt.encode(
-                payload,
-                app.config.get('SECRET_KEY'),
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
-
-    @staticmethod
-    def decode_auth_token(auth_token):
-        """
-        Validates the auth token
-        :param auth_token:
-        :return: integer|string
-        """
-        try:
-            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
-            if is_blacklisted_token:
-                return 'Token blacklisted. Please log in again.'
-            else:
-                return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
-
-
-class Task(db.Model):
-    """ Task Model for storing tasks for users """
-    __tablename__ = "tasks"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    u_id = db.Column(db.Integer, unique=False, nullable=False)
-    task = db.Column(db.String(255), nullable=False)
-    status = db.Column(db.Boolean, nullable=False, default=False)
-
-    def __init__(self, u_id, task, status=False):
-        self.u_id = u_id
-        self.task = task
-        self.status = status
-
-
-class BlacklistToken(db.Model):
-    """
-    Token Model for storing JWT tokens
-    """
-    __tablename__ = 'blacklist_tokens'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    token = db.Column(db.String(500), unique=True, nullable=False)
-    blacklisted_on = db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, token):
-        self.token = token
-        self.blacklisted_on = datetime.datetime.now()
-
-    def __repr__(self):
-        return '<id: token: {}'.format(self.token)
-
-    @staticmethod
-    def check_blacklist(auth_token):
-        # check whether auth token has been blacklisted
-        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
-        if res:
-            return True
-        else:
-            return False
-
 
 if __name__ == '__main__':
     app.run()
